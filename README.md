4ff56f0d6308a6c278f99a6cb6cbfab5759e2680
Raphael Guntersweiler <raphael@rgunti.ch>
2018-11-25 03:24:22 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# CarPi-Commons
+A library providing utilities for writing CarPi programs and services.
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+CARPI COMMONS
+(C) 2018, Raphael "rGunti" Guntersweiler
+Licensed under MIT
+"""
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='carpi-settings',
+      version='0.1.0',
+      description='A library providing utilities for storing settings in a CarPi application.',
+      long_description=long_description,
+      url='https://github.com/rGunti/CarPi-Settings',
+      keywords='carpi app settings',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6'
+      ],
+      author='Raphael "rGunti" Guntersweiler',
+      author_email='raphael@rgunti.ch',
+      license='MIT',
+      packages=['carpisettings'],
+      install_requires=[
+          'carpi-commons',
+          'wheel'
+      ],
+      zip_safe=False,
+      include_package_data=True)
